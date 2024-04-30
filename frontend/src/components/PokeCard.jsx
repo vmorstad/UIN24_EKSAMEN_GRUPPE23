f9ac744195081ea0bd646179e138f248d9ccd468
havardliebe <104149882+havardliebe@users.noreply.github.com>
2024-04-30 16:31:38 +0200

    Update PokeCard.jsx
    henter inn riktig bilde og id for pokemon

--- frontend/src/components/PokeCard.jsx
+++ frontend/src/components/PokeCard.jsx
@@ -1,21 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 export default function PokeCard() {
-  const [pokemonNames, setPokemonNames] = useState([]);
+  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/pokemon-form/')
       .then(response => response.json())
       .then(data => {
-        const names = data.results.map(pokemon => pokemon.name);
-        setPokemonNames(names);
+        const modifiedData = data.results.map(pokemon => ({
+          name: pokemon.name,
+          id: pokemon.url.split('/').slice(-2, -1)[0], // Henter id fra URL
+          picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/').slice(-2, -1)[0]}.png` // Lager bilde-URL basert på id
+        }));
+        setPokemonData(modifiedData);
       })
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
 
   return (
     <>
-      {pokemonNames.map((name, index) => (
-          <h3>{name}</h3>
+      {pokemonData.map(pokemon => (
+        <div key={pokemon.id}>
+          <h3>Name: {pokemon.name}</h3>
+          <p>ID: {pokemon.id}</p>
+          <img src={pokemon.picture} alt={pokemon.name} />
+        </div>
       ))}
     </>
   );
