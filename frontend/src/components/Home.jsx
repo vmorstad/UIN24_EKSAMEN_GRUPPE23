3af6e64694e56be1a26e2e96b7a02d8c3a3f2e05
havardliebe <104149882+havardliebe@users.noreply.github.com>
2024-05-10 12:50:08 +0200

    lagde komponent Pokemon og gjorde endringer på App.scss og Type.jsx
    Lagde en ufullstendig komponent Pokemon.jsx, som henter ut nødvendig data fra api. endret på Type.jsx for å kunne hente ut poison.png. Gjorde styling på Pokemon komponent (ikke ferdig med det).

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import PokeCard from './PokeCard'
 import TypeCard from './TypeCard'
 import Type from './Type'
+import Pokemon from './Pokemon';
 
 export default function Home() {
 
@@ -9,7 +10,8 @@ export default function Home() {
     <>
     <PokeCard limit={9} />
     <TypeCard />
-    <Type type={2}/> {/* 1 = normal, 2 = fighting */}
+    <Pokemon id={145}/>
+    <Type type={4}/> {/* 1 = normal, 2 = fighting */}
     </>
   );
 }
--- frontend/src/components/Type.jsx
+++ frontend/src/components/Type.jsx
@@ -11,7 +11,7 @@ const Type = ({ type }) => {
         const response = await fetch(`https://pokeapi.co/api/v2/type/${type}`)
         const data = await response.json()
         
-        const modifiedData = data.pokemon.map(pokemon => ({
+        const modifiedData = data.pokemon.map(pokemon => ({ //henter ut navn, id og png av pokemon med samme type
           name: pokemon.pokemon.name,
           id: pokemon.pokemon.url.split('/').slice(-2, -1)[0],
           picture: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.pokemon.url.split('/').slice(-2, -1)[0]}.png`
@@ -19,7 +19,9 @@ const Type = ({ type }) => {
   
         setPokemonData(modifiedData)
         setTypeName(data.name)
-        setTypeImage(`https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/${data.name}.png`) //henter type bilde
+        setTypeImage(data.name === 'poison' ? //henter ut png for type
+        `https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/poisen.png` :
+        `https://raw.githubusercontent.com/ackarlse/UIN24_eksamen/master/ressurser/type%20symboler/${data.name}.png`) //henter type bilde
       }
   
       fetchData()
