00b30731edaf95d24857639d0f95862fc3f801ec
vmorstad <viljar.morstad2000@gmail.com>
2024-05-10 14:40:30 +0200

    Fullstendig Teams component
    Teams komponent med routing ferdig.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './styles/App.scss'
 import React from 'react';
+import { Route, Routes } from 'react-router-dom'
 import Layout from '../src/components/Layout';
 import Home from './components/Home';
-import { Route, Routes } from 'react-router-dom'
+import Teams from './components/Teams';
 import Type from './components/Type';
 import Pokemon from './components/Pokemon';
+
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
     <Layout/>
       <Routes>
         <Route path="/home" element={<Home/>}/>
+        <Route path='/teams' element={<Teams/>}/>
         <Route path="/type" element={<Type type={4}/>}/>
         <Route path="/pokemon" element={<Pokemon id={145}/>}/>
       </Routes>
